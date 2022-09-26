5065aab8bd118e5d4a07cb8543ff305733f92a2a
astokarev <sandrotokarev@gmail.com>
2022-09-26 15:32:17 +0300

    second

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("Саша", "Токарев", (byte) 78);
+        userService.saveUser("Олег", "Ляшин", (byte) 74);
+        userService.saveUser("Барак", "Обама", (byte) 59);
+        userService.saveUser("Игорь", "Прокопенко", (byte) 74);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
